37b4dd495f8ff22719885c669bdd8ec7fe4003de
Vitaliy <flatronsamatron@gmail.com>
2020-03-05 12:18:46 +0200

    init: morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,37 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = []
+let arr2 = []
+let x = []
+let str = []
+
+for(let i = 0; i<expr.length; i=i+10){
+	arr.push(expr.substr(i, 10))
+}
+
+
+for(let i = 0; i<arr.length; i++){
+	for(let j = 0; j<arr[i].length;j=j+2){
+  	x.push(arr[i].substr(j, 2))
+    if(x.length==5){arr2.push(x); x = []}
+  } 
+}
+
+
+let arr3 = arr2.map(el=>el.filter(el=>el !== '00')).map(el=>el.map(el=>el=='10' ? '.' : el=='11'? '-' : '**')).map(el=>el.join(''))
+
+
+for(let i=0; i<=arr3.length; i++){
+	for(let j in MORSE_TABLE){
+  	if(arr3[i] == j){str.push(MORSE_TABLE[j])}
+}
+}
+return str.join('')
 }
 
 module.exports = {
